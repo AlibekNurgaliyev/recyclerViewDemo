28b719059725eb4f9e8f09adc181cba539ad6f4f
AlibekNurgaliyev <alibek.nurgaliyev@gmail.com>
2021-08-05 19:13:22 +0600

    added fragments 2

--- app/src/main/java/com/example/myapplication555/fragments/AddToDoDialog.kt
+++ app/src/main/java/com/example/myapplication555/fragments/AddToDoDialog.kt
@@ -5,16 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
 import com.example.myapplication555.R
 
-class SplashFragment : Fragment() {
+class AddToDoDialog : DialogFragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_to_do_dialog, container, false)
     }
 }
--- app/src/main/java/com/example/myapplication555/fragments/RecyclerFragment.kt
+++ app/src/main/java/com/example/myapplication555/fragments/RecyclerFragment.kt
@@ -7,14 +7,16 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.myapplication555.R
 
-class SplashFragment : Fragment() {
+class RecyclerFragment : Fragment() {
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_recycler, container, false)
     }
+
+
 }
